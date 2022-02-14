4f22eb8134bd6daebac4a8f75b62edd3af72e875
J V <JuVelez@mec.cuny.edu>
2022-02-14 13:04:52 -0500

    with challenge

--- src/Components/GridBox.jsx
+++ src/Components/GridBox.jsx
@@ -1,6 +1,10 @@
 import Grid from '@mui/material/Grid';
 import {useState} from 'react';
 
+/* CHALLENGE PART A: Change the code below to render the name of the 
+  background color when the mouse hovers over our GridBox
+   CHALLENGE PART B: conditionally render a component */
+
 export default function GridBox() {
 
   /* use a state hook to change size of GridBox */
@@ -8,20 +12,23 @@ export default function GridBox() {
 
   /* create function to run when GridBox is clicked */
   const toggleSize = (event) => {
-    console.log(event.altKey);
+    console.log(event);
     if(event.altKey) {
       setSize(size - 1); // decrease size up to 12
     } else {
-      setSize(size + 1) ; // increase size up to 12
+      setSize(size + 1); // increase size up to 12
     }
     console.log(`toggleSize() called: size now ${size}`);
   };
 
+  let colorName = <span>orange</span>;
   return (
 	  <Grid item xs={size} >
 			<div className='item'
            onClick={toggleSize}
+           onMouseOver={toggleSize}
       >
+
       </div>
 	  </Grid>
   )
